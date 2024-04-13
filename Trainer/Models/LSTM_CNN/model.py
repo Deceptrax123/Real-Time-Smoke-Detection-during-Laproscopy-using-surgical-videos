a1c7add0aea1905618e149bcd941a5921aa234ba
Zahran Sajid <99584879+zahransajid@users.noreply.github.com>
2024-04-13 06:20:58 +0530

    LSTM_CNN model implemented

--- Trainer/Models/LSTM_CNN/model.py
+++ Trainer/Models/LSTM_CNN/model.py
@@ -1,28 +1,45 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torchvision.models import resnet101
+from torchvision.models import resnet101, ResNet101_Weights
+from torchinfo import summary
+import math
 
 class CNNLSTM(nn.Module):
     def __init__(self):
         super(CNNLSTM, self).__init__()
-        self.resnet = resnet101(pretrained=True)
-        self.resnet.fc = nn.Sequential(nn.Linear(self.resnet.fc.in_features, 10))
-        self.lstm = nn.LSTM(input_size=10, hidden_size=256, num_layers=3)
-        self.fc1 = nn.Linear(256, 128)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=(3,3), stride=1, padding=1)
+        self.act1 = nn.ReLU()
+        self.drop1 = nn.Dropout(0.3)
+ 
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=(3,3), stride=1, padding=1)
+        self.act2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(kernel_size=(2, 2))
+        self.flatten = nn.Flatten()
+        self.lstm = nn.LSTM(input_size=2097152, hidden_size=64, num_layers=1)
+        self.fc1 = nn.Linear(64, 128)
         self.fc2 = nn.Linear(128, 1)
        
     def forward(self, x_3d):
         hidden = None
         # Iterate over each frame of a video in a video of batch * frames * channels * height * width
         for t in range(x_3d.size(1)):
-            with torch.no_grad():
-                x = self.resnet(x_3d[:, t])  
-            # Pass latent representation of frame through lstm and update hidden state
-            out, hidden = self.lstm(x.unsqueeze(0), hidden)         
+            x = x_3d[:,t]
+            x = self.conv1(x)
+            x = self.act1(x)
+            x = self.drop1(x)
+            x = self.conv2(x)
+            x = self.act2(x)
+            x = self.pool2(x)
+            x = self.flatten(x)
+            out, hidden = self.lstm(x, hidden)       
 
         # Get the last hidden state (hidden is a tuple with both hidden and cell state in it)
         x = self.fc1(hidden[0][-1])
         x = F.relu(x)
         x = self.fc2(x)
         return x, F.sigmoid(x)
+
+if __name__ == '__main__':
+    model = CNNLSTM().cuda()
+    summary(model,(8,10,3,512,512))
